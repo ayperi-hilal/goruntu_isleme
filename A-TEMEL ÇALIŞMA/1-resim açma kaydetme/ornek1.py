64364ed26dbdd45705603b48177cf0f772d1413b
hilal <62089994+ayperi-hilal@users.noreply.github.com>
2021-10-28 11:00:23 +0300

    Add files via upload

---
+++ A-TEMEL ÇALIŞMA/1-resim açma kaydetme/ornek1.py
@@ -0,0 +1,11 @@
+import cv2
+##import numpy as np
+##yorum satırı için alr gr+3 yapılır.
+##img=cv2.imread('elma.jpg')
+##cv2.imshow('elma.jpg')
+##img=cv2.imread('elma.jpg',cv2.IMREAD_COLOR)
+img=cv2.imread('elma.jpg')
+####print(img)
+##px=img[50,50]
+##px=[255,255,255]
+cv2.imshow("image",img)
---
+++ A-TEMEL ÇALIŞMA/1-resim açma kaydetme/ornek2.py
@@ -0,0 +1,4 @@
+import cv2
+img=cv2.imread('elma.jpg')
+##imread ile resim okunur.imshow ile de resim gösterilmektedir.
+cv2.imshow("image",img)
